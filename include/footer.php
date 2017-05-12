c82ec4c75d7beba2feb69d54c2981530270bbd6f
Gronberg <gronberg@gmail.com>
2017-05-12 08:40:25 -0700

    google analytics and heroku updates

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 				</div>
 				<div class="spacer"></div>
 			</div>
-			<div id="footer">&copy; <? echo date(Y); ?> Dartmouth Outing Club of Northern California, All Rights Reserved.</div>
+			<div id="footer">&copy; <? echo date("Y"); ?> Dartmouth Outing Club of Northern California, All Rights Reserved.</div>
         </div>
 	</body>
 </html>
